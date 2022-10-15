8cab54baf7087cddda1ded1aaa043f1b9a6028c6
Ariel Gueta <arielguata@gmail.com>
2022-10-15 22:40:49 +0300

    now can communicate

--- SEC.js
+++ SEC.js
@@ -1,69 +1,141 @@
-const msgType = { key: 'key', data: 'data' };
-Object.freeze(msgType);
-
 class SEC {
-	// instance variables
-	#keyChain = {}; // TODO: {target client name: (hold key)}
-	#name = '';
-	#tempKeyHolder = '';
+	// instance variables (secrets)
+	#name = ''; // instance name in the #clients.
+	#keyChain = {}; // hold all the keys for each client that connected with me (this instance).
+	#callbackFunctions = {}; // Hold callback function per client name.
+	#tempKeyHolder = ''; // hold temp values and change per stage of the full key building process.
 
-	// class variables
-	static #p = BigInt(7919); // public (known to everyone) // TODO: make static private
-	static #g = BigInt(3); // public (known to everyone) // TODO: make static private
-	static #clients = {}; // TODO: {name: id for initial event (for keys swap)}
+	// shared variables
+	static #p = BigInt(524287); // TODO: find a better way to get bigger key
+	static #g = BigInt(3);
+	static #clients = {}; // list of all client (SEC instances)
+	static #msgType = { key: 'key' };
 
 	constructor(name) {
 		this.#tempKeyHolder = null;
-		if (typeof name !== 'string') SEC.#throwError('Wrong type of name .', 'string', typeof name);
+		if (typeof name !== 'string') this.#throwError('Wrong type of name .', 'string', typeof name);
 		this.#name = name;
-		SEC.generateClient(name);
-		window.addEventListener(SEC.#clients[name] + msgType.key, (e) => this.#completeConnection(e));
+		SEC.registerClient(name);
+		window.addEventListener(SEC.#clients[name] + SEC.#msgType.key, (e) =>
+			this.#completeConnection(e),
+		);
+		Object.freeze(this);
+		console.log(this);
 	}
-	static generateClient(name) {
+
+	/**
+	 * Couple name and unique id for future connection.
+	 * @param {string} name name of the client
+	 */
+	static registerClient(name) {
+		if (SEC.#clients[name])
+			this.#throwError(
+				`The name ${name} already exist.\nCannot use the same name for more then one instance of SEC.`,
+			);
 		SEC.#clients[name] = Math.round(Math.random() * 1000000000000000).toString();
 	}
 
-	connect(target) {
-		this.#sendHalfKey(target);
+	/**
+	 * Swap keys with specific client.
+	 * @param {string} name name of the client.
+	 */
+	connect(name) {
+		if (this.#keyChain[name]) this.#throwError(`Already connected to this client name (${name})`);
+		if (name === this.#name) this.#throwError(`Client cannot connect to himself.`);
+		this.#sendHalfKey(name);
 	}
 
+	/**
+	 * set callback function to invoke once event is received from client with the specified name.
+	 * @param {string} name client name (specific instance).
+	 * @param {function} callback function to invoke once event is received.
+	 */
+	setEventRespond(name, callback) {
+		if (typeof name !== 'string')
+			this.#throwError(`Expect to different typeof`, 'string', typeof name);
+		if (typeof callback !== 'function')
+			this.#throwError(`Expect to different typeof`, 'function', typeof callback);
+
+		this.#callbackFunctions[name] = callback;
+	}
+
+	/**
+	 * @param {string} name targeted client
+	 * @param {object} data
+	 */
+	send(name, data) {
+		if (!SEC.#clients[name]) this.#throwError(`Can't find client with this name: ${name}.`);
+		if (!this.#keyChain[name]) this.#throwError(`Not connected to client with this name: ${name}.`);
+		if (typeof data !== 'object')
+			this.#throwError(`Expect to different typeof`, 'object', typeof data);
+
+		const eventType = this.#keyChain[name] ^ BigInt(SEC.#clients[name]);
+		const details = data;
+		details.owner = this.#name;
+		const msg = new CustomEvent(eventType, { detail: details, cancelable: false });
+		window.dispatchEvent(msg);
+	}
+
+	/**
+	 * Trigger as a callback after receiving request to connect
+	 * and build full-key with the other client for future communication.
+	 *
+	 * The secret event type is: sharedKey ^ uniqueClientId + 'data'.
+	 * (this.#keyChain[e.detail.owner] ^ (BigInt(SEC.#clients[this.#name]) + SEC.#msgType.data)
+	 * @param {Event} e
+	 */
 	#completeConnection(e) {
-		// TODO: check scenario when this function is called twice (two different connection calls)
-		console.log(`received message:`, e);
 		if (!this.#tempKeyHolder) {
-			// TODO: check if typeof is bigInt
 			this.#sendHalfKey(e.detail.owner);
-			window.removeEventListener(SEC.#clients[this.#name] + msgType.key, (e) =>
-				this.#completeConnection(e),
-			);
 			this.#tempKeyHolder = SEC.#generateFullKey(this.#tempKeyHolder, e.detail.key);
 		} else {
 			this.#tempKeyHolder = SEC.#generateFullKey(e.detail.key, this.#tempKeyHolder);
 		}
 		this.#keyChain[e.detail.owner] = this.#tempKeyHolder;
 		this.#tempKeyHolder = null;
-		this.debugLogs(this.#name); // for debugging
 
-		window.addEventListener(SEC.#clients[this.#name] + msgType.data, (e) =>
-			console.log(e.detail.data),
-		);
+		const eventType = this.#keyChain[e.detail.owner] ^ BigInt(SEC.#clients[this.#name]);
+
+		const callBackFunction = (e) => {
+			console.log('');
+			const callback = this.#callbackFunctions[e.detail.owner];
+			console.log(this.#name + ' received message:');
+			if (callback) {
+				callback(e);
+			} else {
+				console.log(`Cant find callBack function for this client name. \n received event: `, e);
+			}
+		};
+		window.addEventListener(eventType, callBackFunction);
 	}
 
-	#sendHalfKey(target) {
+	/**
+	 * Send event to unique id of specific client name.
+	 * The event.details hold this instance half-key and this instance name.
+	 * @param {string} name client name
+	 */
+	#sendHalfKey(name) {
 		this.#tempKeyHolder = SEC.#generateHalfKey(SEC.#randomNum(11, 99));
-		const data = { key: this.#tempKeyHolder, owner: this.#name };
-		const event = new CustomEvent(SEC.#clients[target] + msgType.key, {
+		const data = { owner: this.#name, key: this.#tempKeyHolder };
+		const event = new CustomEvent(SEC.#clients[name] + SEC.#msgType.key, {
 			detail: data,
+			cancelable: false,
 		});
 		window.dispatchEvent(event);
 	}
 
 	static #generateHalfKey(a) {
+		if (typeof a != 'bigint')
+			this.#throwError('Input should be of type bigint', 'bigint', typeof a);
 		const key = SEC.#g ** a % SEC.#p;
 		return key;
 	}
 
 	static #generateFullKey(a, b) {
+		if (typeof a != 'bigint')
+			this.#throwError('Input should be of type bigint', 'bigint', typeof a);
+		if (typeof a != 'bigint')
+			this.#throwError('Input should be of type bigint', 'bigint', typeof b);
 		const key = a ** b % SEC.#p;
 		console.log(`a: ${a}, b: ${b}, key: ${key}`);
 		return a ** b % SEC.#p;
@@ -72,21 +144,18 @@ class SEC {
 	static #randomNum(min, max) {
 		return BigInt(Math.round(Math.random() * (max - min + 1) + min));
 	}
-	static #throwError(subject, expected, received) {
-		throw Error(`${subject}\nexpected: ${expected}\nreceived: ${received}`);
-	}
 
-	debugLogs(name) {
-		let spaces = '';
-		for (let i = 0; i < 100; i++) {
-			spaces += '	';
-		}
-		console.log(`%c${spaces}`, 'background-color: darkblue;');
-		console.log(
-			'%c Secret-events-communication instance logs ',
-			'background-color: green; color: white',
-		);
-		console.log(this);
-		console.log(`%c${spaces}`, 'background-color: darkblue;');
+	/**
+	 * @param {string} subject  brief
+	 * @param {string} expected expected value (optional)
+	 * @param {string} received real value (optional)
+	 */
+	#throwError(subject, expected, received) {
+		let errMsg = `Client name: ${this.#name}`;
+		if (errMsg) errMsg += `\n${subject}`;
+		if (expected && received) errMsg += `\nexpect: ${expected}\nreceived: ${received}`;
+		throw Error(errMsg);
 	}
 }
+Object.freeze(SEC);
+Object.freeze(SEC.prototype);
--- test.js
+++ test.js
@@ -1,5 +1,31 @@
-const client1 = new SEC('ariel1');
-const client2 = new SEC('ariel2');
-client1.connect('ariel2');
+const client1 = new SEC('client1');
+SEC.prototype.connect = (n) => console.log(n); // check Object.freeze(SEC)
+const client2 = new SEC('client2');
+const client3 = new SEC('client3');
 
-console.dir(SEC);
+client1.connect('client2');
+client3.connect('client2');
+
+client2.setEventRespond('client1', (e) => {
+	console.log('msg type: ', e.type);
+	console.log('got msg:', e.detail);
+});
+client2.setEventRespond('client3', (e) => {
+	console.log('msg type: ', e.type);
+	console.log('1+3=5 nahh its not');
+	console.log(e.detail.msg);
+	a = e.detail.msg;
+});
+client1.setEventRespond('client2', (e) => {
+	console.log('msg type: ', e.type);
+	console.log('got msg:', e.detail);
+});
+client1.setEventRespond('client2', (e) => {
+	console.log('msg type: ', e.type);
+	console.log('got msg:', e.detail);
+});
+
+client1.send('client2', { msg: 'hey' });
+client3.send('client2', { msg: 'wazzaaaaaup my dude1' });
+client2.send('client1', { msg: 'wazzaaaaaup my dude2' });
+client2.send('client3', { msg: 'wazzaaaaaup my dude3' });
