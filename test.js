79784733af3bd8d34b085ac6dcdfacf2b9b3057e
Ariel Gueta <arielguata@gmail.com>
2022-10-22 14:56:19 +0300

    comments

--- test.js
+++ test.js
@@ -3,26 +3,29 @@ SEC.prototype.connect = (n) => console.log(n); // check Object.freeze(SEC)
 const client2 = new SEC('client2');
 const client3 = new SEC('client3');
 
+// client1 connect to client 2
 client1.connect('client2');
+
+// client3 connect to client 2
 client3.connect('client2');
 
+// callback if client2 get event from client1
 client2.setEventRespond('client1', (e) => {
 	console.log('msg type: ', e.type);
 	console.log('got msg:', e.detail);
 });
 
+// callback if client1 get event from client2
 client1.setEventRespond('client2', (e) => {
 	console.log('msg type: ', e.type);
 	console.log('got msg:', e.detail);
 });
 
-client1.send('client2', { msg: 'hey' });
-client2.send('client1', { msg: 'wazzaaaaaup my dude2' });
-client3.send('client2', { msg: 'wazzaaaaaup my dude1' });
-client2.setEventRespond('client3', (e) => {
-	console.log('msg type: ', e.type);
-	console.log('1+3=5 nahh its not');
-	console.log(e.detail.msg);
-	a = e.detail.msg;
-});
-client2.send('client3', { msg: 'wazzaaaaaup my dude3' });
+// send event from client1 to client2
+client1.send('client2', { msg: 'test1' });
+
+// send event from client2 to client1
+client2.send('client1', { msg: 'test2' });
+
+// send event from client3 to client2
+client3.send('client2', { msg: 'test3' });
